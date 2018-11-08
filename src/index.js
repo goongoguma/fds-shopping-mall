65eedd71e96d249ed81d6a33cc13f6ada1b01fcc
goongoguma <wogus7an@gmail.com>
2018-11-08 16:59:02 +0900

    홈 버튼 만들기

--- src/index.js
+++ src/index.js
@@ -69,70 +69,84 @@ async function drawProductsList() {
   // 페이지 그리는 함수 작성 순서
   const frag = document.importNode(templates.productsUl, true);
   const productsListEl = frag.querySelector(".products-list");
-  const homeEl = frag.querySelector('.home');
+  const homeEl = frag.querySelector(".home-button");
   const laptopEl = frag.querySelector(".laptop");
   const tabletEl = frag.querySelector(".tablet");
   const consoleEl = frag.querySelector(".console-game");
   const phoneEl = frag.querySelector(".phone");
-
+  const categoryList = frag.querySelector('.category-list')
+  homeEl.addEventListener('click', e => {
+    rootEl.textContent='';
+    drawProductsList();
+  })
   /******** Laptop 페이지 ********/
   // 페이지 그리는 함수 작성 순서
   // 1. 템플릿 복사
 
   async function drawCategory(category) {
-    if(category === 'home') {
-      productsListEl.textContent = '';
-    }
     const res = await api.get(`/products?category=${category}`);
     // res.data[0].mainImgUrl
-    productsListEl.textContent = '';
+    productsListEl.textContent = "";
     for (let i = 0; i < res.data.length; i++) {
       const frag = document.importNode(templates.productsLi, true);
-      const productsLiEl = frag.querySelector('.productsLiEl');
-      const imageEl = frag.querySelector('.image');
+      const productsLiEl = frag.querySelector(".productsLiEl");
+      const imageEl = frag.querySelector(".image");
       imageEl.src = res.data[i].mainImgUrl;
+      // console.log(res.data[i].id)
       productsLiEl.appendChild(imageEl);
       productsListEl.appendChild(frag);
+
+      productsLiEl.addEventListener("click", e => {
+        information(res.data[i].id);
+        productsListEl.textContent = "";
+        categoryList.textContent = '';
+      });
     }
   }
 
-
-  homeEl.addEventListener("click", e => {
-    drawCategory('home');
-  });
   laptopEl.addEventListener("click", e => {
-    drawCategory('Laptop');
+    drawCategory("Laptop");
   });
   tabletEl.addEventListener("click", e => {
-    drawCategory('Tablet');
+    drawCategory("Tablet");
   });
   consoleEl.addEventListener("click", e => {
-    drawCategory('ConsoleGame');
+    drawCategory("ConsoleGame");
   });
   phoneEl.addEventListener("click", e => {
-    drawCategory('Phone');
+    drawCategory("Phone");
   });
 
+  rootEl.appendChild(frag);
+}
+
+drawProductsList();
+
 /*************************** 세부화면 상품 ***************************/
-// async function information(liId) {
-//   const res = await api.get("/products");
-//   const list = res.data;
+async function information(liId) {
+  const res = await api.get("/products");
+  const list = res.data;
 
-//   // 페이지 그리는 함수 작성 순서
-//   // 1. 템플릿 복사
-//   const frag = document.importNode(templates.productsInfo, true);
-//   // 2. 요소 선택
-//   const infoEl = frag.querySelector('.infoEl');
+  // 페이지 그리는 함수 작성 순서
+  // 1. 템플릿 복사
+  const frag = document.importNode(templates.productsInfo, true);
+  // 2. 요소 선택
+  const infoEl = frag.querySelector('.infoEl');
+  const homeEl = frag.querySelector(".home-button");
+  homeEl.addEventListener('click', e => {
+    rootEl.textContent='';
+    drawProductsList();
+  })
 
-//   // 3. 필요한 데이터 불러오기
-//   // 4. 내용 채우기
-//   if(liId) {
-//     const res = await api.get(`/products/${liId}/`);
-//     const detailImgUrls = res.data.detailImgUrls
-//     infoEl.setAttribute('src', detailImgUrls[0]);
-//     frag.appendChild(infoEl);
-//   }
-//   // 5. 이벤트 리스너 등록하기
-//   // 6. 템플릿을 문서에 삽입
-//   rootEl.appendChild(frag);
-// }
+
+  // 3. 필요한 데이터 불러오기
+  // 4. 내용 채우기
+  if(liId) {
+    const res = await api.get(`/products/${liId}/`);
+    const detailImgUrls = res.data.detailImgUrls
+    infoEl.setAttribute('src', detailImgUrls[0]);
+  }
+  // 5. 이벤트 리스너 등록하기
+  // 6. 템플릿을 문서에 삽입
+  rootEl.appendChild(frag);
+}
